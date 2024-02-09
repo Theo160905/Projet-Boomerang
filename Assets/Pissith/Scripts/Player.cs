0464b17e707e6b37a4b216a9bbc1177da5f3188f
Theo160905 <145351781+Theo160905@users.noreply.github.com>
2024-02-09 11:20:25 +0100

    Boomerang qui marche pas

--- Assets/Pissith/Scripts/Player.cs
+++ Assets/Pissith/Scripts/Player.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
-}
+    private bool isRecup = true;
 
+    [SerializeField]
+    private UseBoomerang useBoomerang;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isRecup == true)
+        {
+            useBoomerang.ThrowBoommerang();
+        }
+    }
+
+}

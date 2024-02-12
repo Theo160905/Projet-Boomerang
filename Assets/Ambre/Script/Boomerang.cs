61956319474f65e1ad71b1703545a0eadc9374ba
Theo160905 <145351781+Theo160905@users.noreply.github.com>
2024-02-12 11:02:55 +0100

    Fix Bug

--- Assets/Ambre/Script/Boomerang.cs
+++ Assets/Ambre/Script/Boomerang.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class Baamrang : MonoBehaviour
+public class Boomerang : MonoBehaviour
 {
     private float time;
 
